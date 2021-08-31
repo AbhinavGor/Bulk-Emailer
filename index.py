527d7fcdad3c3d53a585371c3069be5cb08d66d0
AbhinavGor <abhinav20016@gmail.com>
2021-08-31 23:48:44 +0530

    ADD: SMTP login Code
    - Added basic smtp login code for gmail
    - added functionality to have custom column names for email and name columns in the csv file

--- index.py
+++ index.py
@@ -5,21 +5,24 @@ def run():
     sender_email = str(sender_email_entry.get())
     sender_email_password  = str(sender_email_password_entry.get())
     emails_path_str = str(emails_path_entry.get())
-    
-    os.system(f'python3 mail_bot.py {sender_email} {sender_email_password} {emails_path_str}')
+    email_col_name = str(emails_column_name_entry.get())
+    names_col_name = str(name_column_name_entry.get())
+
+    os.system(f'python3 mail_bot.py {sender_email} {sender_email_password} {emails_path_str} {email_col_name} {names_col_name}')
     
 root = Tk()
 root.geometry('1000x500')
 root.title('Bulk Emailer')
 
-sender_email_label = Label(root, text="Sender email:", width=20)
-sender_email_label.place(x=100, y=100)
+sender_email_label = Label(root, text="Sender email:", width=20).place(x=100, y=100)
 
-sender_email_password_label = Label(root, text="Sender email password:")
-sender_email_password_label.place(x=100, y=150)
+sender_email_password_label = Label(root, text="Sender email password:").place(x=100, y=150)
 
-emails_path = Label(root, text="Emails list (csv) path:")
-emails_path.place(x=100, y=200)
+emails_path = Label(root, text="Emails list (csv) path:").place(x=100, y=200)
+
+emails_column_name = Label(root, text="Email column name:").place(x=100, y=250)
+
+name_column_name = Label(root, text="Name column name:").place(x=100, y=300)
 
 sender_email_entry = Entry(root, width=60)
 sender_email_entry.place(x=300, y=100)
@@ -27,8 +30,10 @@ sender_email_entry.place(x=300, y=100)
 sender_email_password_entry = Entry(root)
 sender_email_password_entry.place(x=300, y=150)
 
-emails_path_entry = Entry(root, width=60)
-emails_path_entry.place(x=300, y=200)
+emails_path_entry = Entry(root, width=60).place(x=300, y=200)
+
+emails_column_name_entry = Entry(root, width=60).place(x=300, y=250)
+name_column_name_entry = Entry(root, width=60).place(x=300, y=300)
 
 Button(root, text="Submit", width=20, command=run).place(x=180, y=380)
 
--- mail_bot.py
+++ mail_bot.py
@@ -1,18 +1,37 @@
-import sys
+import sys, smtplib, mimetypes
 import pandas as pd
 import numpy as np
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.message import EmailMessage
+
+def login_user(email, password):
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.starttls()
+    smtp.login(email, password)
 
 def get_emails(emails_path):
-    print(f'Reading emails from {emails_path}.')
+    print(f'[.] Reading emails from {emails_path}.')
+    emails = pd.read_csv(emails_path)
 
-def send_emails(sender_email, sender_email_password, emails_path):
+    print(f'[!] {len(emails.index)} emails read from {emails_path}.')
+
+    return emails
+
+def send_emails(sender_email, sender_email_password, emails_path, emails_col_name, names_col_name):
+    login_user(sender_email, sender_email_password)
+    print(f'[!] Logged in as {sender_email}.')
+    
     emails_df = get_emails(emails_path)
-    print(f'Using {sender_email} to send emails to {sender_email_password}, {emails_path}')
+
+    print(f'Using {sender_email} to send emails to {len(emails_df.index)} emails')
 
 if __name__ == "__main__":
     sender_email = str(sys.argv[1])
     sender_email_password = str(sys.argv[2])
     emails_path = str(sys.argv[3])
+    emails_col_name = str(sys.argv[4])
+    names_col_name = str(sys.argv[5])
 
-    send_emails(sender_email, sender_email_password, emails_path)
+    send_emails(sender_email, sender_email_password, emails_path, emails_col_name, names_col_name)
